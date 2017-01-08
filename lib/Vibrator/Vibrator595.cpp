d63cb8c499be34f4c5cbb0daeec9edc5e6c83c23
singajeet <singajeet@gmail.com>
2017-01-08 19:12:20 +0530

    More Changes

--- lib/Vibrator/Vibrator595.cpp
+++ lib/Vibrator/Vibrator595.cpp
@@ -12,12 +12,24 @@ Vibrator595::Vibrator595(int pLatchPin, int pDataPin, int pClockPin){
   this->vLatchPin = pLatchPin;
   this->vDataPin = pDataPin;
   this->vClockPin = pClockPin;
+
+  pinMode(this->vLatchPin, OUTPUT);
+  pinMode(this->vDataPin, OUTPUT);
+  pinMode(this->vClockPin, OUTPUT);
+
+  digitalWrite(this->vLatchPin, LOW);
+  shiftOut(this->vDataPin, this->vClockPin, MSBFIRST, 0);
+  digitalWrite(this->vLatchPin, HIGH);
 }
 
 void Vibrator595::vibrateOn(){
-
+  digitalWrite(this->vLatchPin, LOW);
+  shiftOut(this->vDataPin, this->vClockPin, MSBFIRST, ((1 << 5) - 1));
+  digitalWrite(this->vLatchPin, HIGH);
 }
 
 void Vibrator595::vibrateOff(){
-  
+  digitalWrite(this->vLatchPin, LOW);
+  shiftOut(this->vDataPin, this->vClockPin, MSBFIRST, 0);
+  digitalWrite(this->vLatchPin, HIGH);
 }
